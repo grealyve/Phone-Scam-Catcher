190a4dc129b048d0a0396f399388ef751d29b8ff
Yusuf Yildiz <41903311+grealyve@users.noreply.github.com>
2023-12-30 14:01:21 +0300

    web3j

--- src/main/java/com/phoneScamCatcher/scatcher/ScatcherApplication.java
+++ src/main/java/com/phoneScamCatcher/scatcher/ScatcherApplication.java
@@ -2,12 +2,15 @@ package com.phoneScamCatcher.scatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
 @SpringBootApplication
 public class ScatcherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScatcherApplication.class, args);
+		Web3j web3j = Web3j.build(new HttpService());
 	}
 
 }
